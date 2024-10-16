3f0f80e33f2723062e4debcac4f38a60d8c1719f
SaidZamora <152555352+SaidZamora@users.noreply.github.com>
2024-10-15 21:11:43 -0600

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+# Plot 1
+# Read the data
+data <- read.table(file.choose(), header = TRUE, sep = ";", na.strings = "?")
+
+# Convert the Date and Time variables to Date/Time classes
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- strptime(data$Time, format = "%H:%M:%S")
+
+# Subset the data for the dates 2007-02-01 and 2007-02-02
+data_subset <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+# Create the histogram
+png("plot1.png", width = 480, height = 480)
+hist(data_subset$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+dev.off()
